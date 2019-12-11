6071f3f9aeaa6975af1825c1742ffa82bb3dc432
Ivo Verberk <iverberk@xebia.com>
2019-12-12 00:14:35 +0100

    Day 10 solutions.

---
+++ day-10/part2.py
@@ -0,0 +1,38 @@
+import math
+
+astroids = []
+
+with open('input') as f:
+    for y, line in enumerate(f):
+        for x, loc in enumerate(line.strip()):
+            if loc == '#':
+                astroids.append((x, y))
+
+max_targets = 0
+station = None
+for astroid in astroids:
+    x, y = astroid
+    targets = {}
+
+    for target in astroids:
+        if astroid == target:
+            continue
+
+        tx, ty = target
+        dx, dy = tx - x, ty - y
+
+        degrees = 90 + math.degrees(math.atan2(dy, dx))
+        if degrees < 0:
+            degrees += 360
+
+        targets[degrees] = target
+
+    if len(targets) > max_targets:
+        station = {'position': astroid, 'targets': targets}
+        max_targets = len(targets)
+
+degrees = sorted(station['targets'])
+
+# No need for laser rotation, we already have
+# 200 astroids in the first pass.
+print(station['targets'][degrees[199]])
